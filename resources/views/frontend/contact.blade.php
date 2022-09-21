1c1f0e7fd6e685db03e4ed9a26d1160c6774a599
victoriamwangi <victoriamwangi021@gmail.com>
2022-09-21 05:37:32 +0300

    contact and aboutus page

--- resources/views/frontend/contact.blade.php
+++ resources/views/frontend/contact.blade.php
@@ -1,97 +1,68 @@
 @extends('frontend.base')
 
 @section('content')
-
-
-
-
-
-<main id="main" class="main" role="main">
+<main id="main" class="contact-main pt-3" role="main">
     <article id="post-15" class="post-15 page type-page status-publish hentry">
 
-        <div class="entry-content">
-            <h1 class="entry-title text-left">Contact Freelancer's Digest</h1>
-            <p>
-                <img class="avatar alignright size-thumbnail"
-                    src="https://freelancersdigest.com/wp-content/uploads/2021/12/cropped-me.jpg" alt="cate" width="150"
-                    height="150" data-pin-nopin="true">
-            </p>
-            <p class="null">Find me on social:&nbsp;&nbsp;
+        <div class="">
+            <h1 class="entry-title text-left">Contact Catherine</h1>
 
-                <a class="mx-1" style="color: inherit; border: 0;" href="" target="_blank" rel="noopener">
-                    <img loading="lazy"
-                        src="https://elnacain.com/wp-content/themes/elna-v5/images/icons/fa-facebook-f.svg" width="20"
-                        height="20" data-pin-nopin="true"></a>
-                <a class="mx-1" style="color: inherit; border: 0;" href="https://twitter.com/ecainwrites"
-                    target="_blank" rel="noopener">
-                    <img loading="lazy" src="https://elnacain.com/wp-content/themes/elna-v5/images/icons/fa-twitter.svg"
-                        width="20" height="20" data-pin-nopin="true"></a>
-                <a class="mx-1" style="color: inherit; border: 0;" href="https://www.instagram.com/elnacain/"
-                    target="_blank" rel="noopener">
-                    <img loading="lazy"
-                        src="https://elnacain.com/wp-content/themes/elna-v5/images/icons/fa-instagram.svg" width="20"
-                        height="20" data-pin-nopin="true"></a>
-            </p>
-            <p>To reach me, just drop me your email at <a
+                    <div class="social-avatar">
+                    <div class="contact-content">
+                    <p class="null">Find me on social:&nbsp;&nbsp;
+
+                    <a class="mx-1" style="color: inherit; border: 0;" href="" target="_blank" rel="noopener">
+                        <img loading="lazy"
+                            src="https://elnacain.com/wp-content/themes/elna-v5/images/icons/fa-facebook-f.svg" width="20"
+                            height="20" data-pin-nopin="true"></a>
+                    <a class="mx-1" style="color: inherit; border: 0;" href="https://twitter.com/ecainwrites"
+                        target="_blank" rel="noopener">
+                        <img loading="lazy" src="https://elnacain.com/wp-content/themes/elna-v5/images/icons/fa-twitter.svg"
+                            width="20" height="20" data-pin-nopin="true"></a>
+                    <a class="mx-1" style="color: inherit; border: 0;" href="https://www.instagram.com/elnacain/"
+                        target="_blank" rel="noopener">
+                        <img loading="lazy"
+                            src="https://elnacain.com/wp-content/themes/elna-v5/images/icons/fa-instagram.svg" width="20"
+                            height="20" data-pin-nopin="true"></a>
+                    </p>
+                    <p>To reach me, just drop me your email at <a
                     href="mailto:freelancersdigest@gmail.com">freelancersdigest@gmail.com</a>. I’ll find it and respond
-                ASAP!</p>
-            <p>You can also follow me on <a
-                    href="https://www.linkedin.com/in/freelance-seo-copywriter-katie-the-writer/">LinkedIn</a> and send
-                me an Inmail and I’ll also respond within 12 hours.</p>
-            <p>Alternatively, you can leave me a WhatsApp message on +254 706 724 231</p>
+                        ASAP!</p>
+                    <p>You can also follow me on <a
+                            href="https://www.linkedin.com/in/freelance-seo-copywriter-katie-the-writer/">LinkedIn</a> and send
+                        me an Inmail and I’ll also respond within 12 hours.</p>
+                    <p>Alternatively, you can leave me a WhatsApp message on +254 706 724 231</p>
 
-            <p>Or send me your questions or comments below and I’ll get back to you as soon as I can!</p>
-            <div class="clearfix"></div>
-            <div role="form" class="wpcf7" id="wpcf7-f19-p15-o1" lang="en-US" dir="ltr">
-                <div
-                    class="screen-reade                                                                                                                                                                                                                                                                                                                                                                                ````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````` r-response">
-                    <p role="status" aria-live="polite" aria-atomic="true"></p>
-                    <ul></ul>
-                </div>
-                <form action="/contact/#wpcf7-f19-p15-o1" method="post" class="wpcf7-form init" novalidate="novalidate"
-                    data-status="init">
-                    <div style="display: none;">
-                        <input type="hidden" name="_wpcf7" value="19"><input type="hidden" name="_wpcf7_version"
-                            value="5.5.6.1"><input type="hidden" name="_wpcf7_locale" value="en_US"><input type="hidden"
-                            name="_wpcf7_unit_tag" value="wpcf7-f19-p15-o1"><input type="hidden"
-                            name="_wpcf7_container_post" value="15"><input type="hidden" name="_wpcf7_posted_data_hash"
-                            value="">
+                    <p>Or send me your questions or comments below and I’ll get back to you as soon as I can!</p>
                     </div>
-                    <p>Name <span style="color:red">*</span><br><span class="wpcf7-form-control-wrap your-name"><input
-                                type="text" name="your-name" value="" size="40"
-                                class="wpcf7-form-control wpcf7-text wpcf7-validates-as-required form-control"
-                                aria-required="true" aria-invalid="false"
-                                style="background-image: url(&quot;data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAYAAABzenr0AAAAAXNSR0IArs4c6QAAAfBJREFUWAntVk1OwkAUZkoDKza4Utm61iP0AqyIDXahN2BjwiHYGU+gizap4QDuegWN7lyCbMSlCQjU7yO0TOlAi6GwgJc0fT/fzPfmzet0crmD7HsFBAvQbrcrw+Gw5fu+AfOYvgylJ4TwCoVCs1ardYTruqfj8fgV5OUMSVVT93VdP9dAzpVvm5wJHZFbg2LQ2pEYOlZ/oiDvwNcsFoseY4PBwMCrhaeCJyKWZU37KOJcYdi27QdhcuuBIb073BvTNL8ln4NeeR6NRi/wxZKQcGurQs5oNhqLshzVTMBewW/LMU3TTNlO0ieTiStjYhUIyi6DAp0xbEdgTt+LE0aCKQw24U4llsCs4ZRJrYopB6RwqnpA1YQ5NGFZ1YQ41Z5S8IQQdP5laEBRJcD4Vj5DEsW2gE6s6g3d/YP/g+BDnT7GNi2qCjTwGd6riBzHaaCEd3Js01vwCPIbmWBRx1nwAN/1ov+/drgFWIlfKpVukyYihtgkXNp4mABK+1GtVr+SBhJDbBIubVw+Cd/TDgKO2DPiN3YUo6y/nDCNEIsqTKH1en2tcwA9FKEItyDi3aIh8Gl1sRrVnSDzNFDJT1bAy5xpOYGn5fP5JuL95ZjMIn1ya7j5dPGfv0A5eAnpZUY3n5jXcoec5J67D9q+VuAPM47D3XaSeL4AAAAASUVORK5CYII=&quot;); background-repeat: no-repeat; background-attachment: scroll; background-size: 16px 18px; background-position: 98% 50%; cursor: auto;"></span>
-                    </p>
-                    <p><span id="wpcf7-631735c19a9db-wrapper" class="wpcf7-form-control-wrap email-wrap"
-                            style="display:none !important; visibility:hidden !important;"><label
-                                for="wpcf7-631735c19a9db-field" class="hp-message">Please leave this field
-                                empty.</label><input id="wpcf7-631735c19a9db-field"
-                                class="wpcf7-form-control wpcf7-text" type="text" name="email" value="" size="40"
-                                tabindex="-1" autocomplete="new-password"></span></p>
-                    <p>Email <span style="color:red">*</span><br><span class="wpcf7-form-control-wrap your-email"><input
-                                type="email" name="your-email" value="" size="40"
-                                class="wpcf7-form-control wpcf7-text wpcf7-email wpcf7-validates-as-required wpcf7-validates-as-email form-control"
-                                aria-required="true" aria-invalid="false"></span> </p>
-                    <p>Subject<br><span class="wpcf7-form-control-wrap your-subject"><input type="text"
-                                name="your-subject" value="" size="40"
-                                class="wpcf7-form-control wpcf7-text form-control" aria-invalid="false"></span> </p>
-                    <p>Message<br><span class="wpcf7-form-control-wrap your-message"><textarea name="your-message"
-                                cols="40" rows="10" class="wpcf7-form-control wpcf7-textarea form-control"
-                                aria-invalid="false"></textarea></span> </p>
-                    <p><input type="submit" value="Send"
-                            class="wpcf7-form-control has-spinner wpcf7-submit btn btn-primary"><span
-                            class="wpcf7-spinner"></span></p>
-                    <p style="display: none !important;"><label>Δ<textarea name="_wpcf7_ak_hp_textarea" cols="45"
-                                rows="8" maxlength="100"></textarea></label><input type="hidden" id="ak_js_1"
-                            name="_wpcf7_ak_js" value="1662465510305">
-                        <script>
-                        document.getElementById("ak_js_1").setAttribute("value", (new Date()).getTime());
-                        </script>
-                    </p>
-                    <div class="wpcf7-response-output" aria-hidden="true"></div>
-                </form>
+                    
+                    <img class="avatar-contact alignright size-thumbnail"
+                        src="https://freelancersdigest.com/wp-content/uploads/2021/12/cropped-me.jpg" alt="cate" width="150"
+                        height="150" data-pin-nopin="true">
+                </div>
+           
+          
+            <div class="clearfix"></div>
+            <form>
+            <div class="form-group">
+                <label for="name">Name</label>
+                <input type="text" class="form-control" id="name" aria-describedby="name" placeholder="Enter your name">
             </div>
+            <div class="form-group">
+                <label for="exampleInputEmail1">Email address</label>
+                <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+                </div>
+            <div class="form-group">
+                <label for="name">Subject</label>
+                <input type="text" class="form-control" id="subject" aria-describedby="subject" placeholder="Enter subject">
+            </div>
+            <div class="form-group">
+                <label for="exampleFormControlTextarea1">Message</label>
+                <textarea class="form-control" id="exampleFormControlTextarea1" rows="6"></textarea>
+            </div>
+            
+            <button type="submit" class="btn btn-primary btn-contact">Submit</button>
+            </form>
 
         </div><!-- .entry-content -->
 
