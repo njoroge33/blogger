117a44208ed6096fa2e6144c1ef421a5045e2f96
victoriamwangi <victoriamwangi021@gmail.com>
2022-10-12 11:20:27 +0300

    services page

--- resources/views/frontend/services.blade.php
+++ resources/views/frontend/services.blade.php
@@ -2,87 +2,149 @@
 
 @section('content')
 <main id="main" class="main services " role="main">
- <p class="large">Boost your revenue with high-quality content for your website
-</p>
-<p class="medium">Hire a content writer who understands your audiences' needs and crafts content that resonates with them.</p>
-<a name="" id="" class="btn btn-primary" href="#" role="button">Book a Call</a>
-<a name="" id="" class="btn btn-primary" href="#" role="button">Send a Message</a>
+    <p class="large title">Boost your revenue with high-quality content for your website
+    </p>
+    <p class="medium title-2">Hire a content writer who understands your audiences' needs and crafts content that resonates with them.</p>
+    <a name=""  id="" class="btn btn-primary book" href="#" role="button">Book a Call</a>
+    <a name=""  id="" class="btn btn-primary send" href="#" role="button">Send a Message</a>
 
-<p class="title">Meet Catherine, Your Go-to Expert Writer for Captivating Website Content</p>
-<p>
-I know your problem--You are sick of the same old content on the net being written, re-written, published, and republished. And I know you don't want that for your website. 
-<br>
-You want content that attracts, engages, convinces, and converts. 
-<br>
+    <div class="container shadow py-2">
+    <p class="section-title ">Meet Catherine, Your Go-to Expert Writer for Captivating Website Content</p>
+    <p>
+       <span> I know your problem--</span><br>You are sick of the same old content on the net being written, re-written, published, and republished. And I know you don't want that for your website.
+        <br>
+        You want content that attracts, engages, convinces, and converts.
+        <br>
 
-You want great content that will rank higher in Google search results and boost your website traffic, but you don't have the time or skills to do it yourself. 
+        You want great content that will rank higher in Google search results and boost your website traffic, but you don't have the time or skills to do it yourself.
 
-</p>
-<p class="title">
-Quality is King 
-</p>
-<p>
-It's no secret that high-quality content is vital for SEO success, but creating great content takes a lot of time and effort.
+    </p>
+    </div>
+    
+    <div class="shadow">
+    
+    <p class="section-title">
+        Quality is King
+    </p>
+    <p>
+        It's no secret that high-quality content is vital for SEO success, but creating great content takes a lot of time and effort.
 
-Not only is it hard to write quality content, but it's also challenging to keep up with the latest SEO trends. If you're not writing your website content correctly and consistently, you're doing your website a disservice and are likely losing potential customers.
-</p>
-<p>
-Get Help from an Experienced Writer
+        Not only is it hard to write quality content, but it's also challenging to keep up with the latest SEO trends. If you're not writing your website content correctly and consistently, you're doing your website a disservice and are likely losing potential customers.
+    </p>
+    </div>
+    <div class="shadow">
 
-</p>
-<p>
-If your plate is full and you have less time to create quality content, worry not--you don't have to do it all by yourself. 
+    <p class="section-title">
+        Get Help from an Experienced Writer
 
-Hiring a skilled and experienced writer can help take care of all your content needs so you can focus on what you're good at - running your business. 
+    </p>
+    <p>
+        If your plate is full and you have less time to create quality content, worry not--you don't have to do it all by yourself.
 
-As an expert writer, I'll work with you to create SEO-optimized website pages and blog posts that will help increase traffic to your website and boost your ranking in Google search results. 
-I'll help you create engaging and informative articles that will keep your readers coming back and convert them into buying customers
+        Hiring a skilled and experienced writer can help take care of all your content needs so you can focus on what you're good at - running your business.
 
-</p>
-<a name="" id="" class="btn btn-primary" href="#" role="button">BOOK A CALL </a>
-<p>
-My Content Writing Services
-</p>
-<ul>
-<li>Blog Post Writing: Engaging Long-form Blog posts</li>
-<li>Website Copywriting: Highly converting Website Copy</li>
-<li>Website Pages: SEO-Optimized Website Pages (e.g., About pages and Service pages)</li>
-<li>Captivating Landing Pages</li>
-<li>Highly-converting Sales Pages</li>
-<li>Proofreading and Copyediting</li>
-<li>Product Reviews</li>
+        As an expert writer, I'll work with you to create SEO-optimized website pages and blog posts that will help increase traffic to your website and boost your ranking in Google search results.
+        I'll help you create engaging and informative articles that will keep your readers coming back and convert them into buying customers
 
-</ul>
+    </p>
+    <a name="" id="" class="btn btn-primary book" href="#" role="button">BOOK A CALL </a>
+    </div>
+    <div class="services-content shadow">
+        <p class="section-title">
+            My Content Writing Services
+        </p>
+        <ul>
+            <li>Blog Post Writing: Engaging Long-form Blog posts</li>
+            <li>Website Copywriting: Highly converting Website Copy</li>
+            <li>Website Pages: SEO-Optimized Website Pages (e.g., About pages and Service pages)</li>
+            <li>Captivating Landing Pages</li>
+            <li>Highly-converting Sales Pages</li>
+            <li>Proofreading and Copyediting</li>
+            <li>Product Reviews</li>
 
-<p>Who Needs a Writer?</p>
-<p>
-Every business on the planet needs a writer. This is because content marketing is the only marketing that's left. 
+        </ul>
+    </div>
+    <div class="shadow">
+    <p class="section-title">Who Needs a Writer?</p>
+    <p>
+        Every business on the planet needs a writer. This is because content marketing is the only marketing that's left.
 
-You need a write if you are any of the following:
-<ul>
-<li>
-eCommerce Business Owners
-<p>
-If you're an eCommerce business owner, your site should have engaging, informative, and persuasive content to convert website visitors into customers. A professional writer can help you create content that makes your site put more money into your pocket. 
-</p>
-</li>
-<li>Website/Blog Owners
-<p>Whether you run a personal blog or a corporate website, you need high-quality content to attract and retain your site visitors. An experienced writer can help you create compelling content that keeps your readers coming back for more. 
-</p>
-</li>
-<li>
-Affiliate Marketers
-<p>If you're an affiliate marketer, your goal is to promote products and services on your website. To do this effectively, you need quality, persuasive content that will persuade people to click on your links. With the help of a tactical writer, you can increase your click-through rates and quickly achieve your affiliate marketing goals. </p>
-</li>
-<li>
-Corporate Business
-<p>
-As a corporate business, your website is often the first impression potential customers get about your company, so it is essential to make a good impression. A professional writer can help you create content that will reflect well on your company and bring you more business.</p>
-</li>
+        You need a write if you are any of the following:
+        <ul>
+            <li>
+                eCommerce Business Owners
+                <p>
+                    If you're an eCommerce business owner, your site should have engaging, informative, and persuasive content to convert website visitors into customers. A professional writer can help you create content that makes your site put more money into your pocket.
+                </p>
+            </li>
+            <li>Website/Blog Owners
+                <p>Whether you run a personal blog or a corporate website, you need high-quality content to attract and retain your site visitors. An experienced writer can help you create compelling content that keeps your readers coming back for more.
+                </p>
+            </li>
+            <li>
+                Affiliate Marketers
+                <p>If you're an affiliate marketer, your goal is to promote products and services on your website. To do this effectively, you need quality, persuasive content that will persuade people to click on your links. With the help of a tactical writer, you can increase your click-through rates and quickly achieve your affiliate marketing goals. </p>
+            </li>
+            <li>
+                Corporate Business
+                <p>
+                    As a corporate business, your website is often the first impression potential customers get about your company, so it is essential to make a good impression. A professional writer can help you create content that will reflect well on your company and bring you more business.</p>
+            </li>
 
-</ul>
-</p>
+        </ul>
+    </p>
+    </div>
+    <div class="faqs container shadow">
+        <p class="faqs-title">FAQs </p>
+        <p>Here are some frequently asked questions about my content writing services-- and the answers.</p>
+        <p class="faqs-question">Which niches do you specialize in?</p>
+        <p>I'm a flexible writer and can write in a variety of niches. However, the niches I specialize in most include:</p>
+        <ol>
+        <li>SaaS</li>
+        <li>Digital Marketing</li>
+        <li>eCommerce</li>
+        <li>Health and wellness</li>
+        <li>Alternative Health</li>
+        <li>Content writing</li>
+        <li>Freelancing</li>
+        </ol>
+        <p>I've also worked with clients on unpopular topics like pets, woodworking, jewelry, and home decor.</p>
+        <p class="faqs-question">Can I see some of your samples?</p>
+        <p>Absolutely! Here's my portfolio (Link to portfolio page) for some of my published articles. 
+        <br>
+        However, if you'd like to see niche-specific samples, contact me, and I'll send them directly to you.
+        </p>
+        <p class="faqs-question">How much does it cost?</p>
+        <p>My content writing services are affordable to fit your needs and budget. 
+        <br>
+        Here's the price list of my most popular services:
+        </p>
+        <ol>
+        <li>Long-form blog post - $100 per 1000-word blog post </li>
+        <li>Website service pages - $100 per page </li>
+        <li>Landing Page Copy - $250 per page </li>
+        <li>About Page or Service Page - $75 per page </li>
+        <li>Sales Copy - $200 per page </li>
+        <li>Product Reviews - $50 per review</li>
+        </ol>
+        <p class="faqs-question">Do you take bulk orders?</p>
+        <p>
+        Yes, I take bulk orders. And the good thing about them is that you'll pay less per article. 
+        <br>For instance, if your order 10 blog posts or more, you pay $85 per 1000 words instead <br> of the usual $100 per 1000 words
+        </p>
+        <p class="faqs-question">Do you do content revision?</p>
+        <p>Yes. If you're not satisfied with what you've received or would like some amendments,<br> just let me know, and I'll fix it for you- For FREE</p>
+        <p class="faqs-question">
+        How do I get started?
 
+        </p>
+        <p>
+        If you're looking for content that will engage your readers and convert them into customers, Book a call or send me a message now, and I'll get back to you in 2 hours. 
+
+        </p>
+        <a name="" id="" class="btn btn-primary book" href="#" role="button">Book a Call</a>
+        <a name="" id="" class="btn btn-primary send" href="#" role="button">Send a Message</a>
+</div>
 </main>
 
 @endsection
