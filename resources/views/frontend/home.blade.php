70eee663258caf9390784a3240c482dd2b272b32
victoriamwangi <victoriamwangi021@gmail.com>
2022-10-13 21:59:35 +0300

    st: changes

--- resources/views/frontend/home.blade.php
+++ resources/views/frontend/home.blade.php
@@ -11,7 +11,7 @@
             <div class="firstsection d-flex justify-content-center text-center">
                 <section class="front-page-optin front-page-optin-gptw ">
 
-                    <p class="fp-optin-title landing-page-h1">Feeling Clueless </br>About Landing Clients? </p>
+                    <p class="fp-optin-title landing-page-h1">Feeling Clueless </br>About Landing  <br>Freelance Writing Jobs? </p>
                     <p class="fp-optin-text">Here’s a <span >FREE 5-step guide</span> to start landing high-paying freelance writing jobs </p>
                     <div class="fp-form-wrap">
                         <div class="clearfix"></div>
@@ -27,7 +27,7 @@
                                             <div class="invalid-feedback">Please enter a valid email.</div>
                                         </div>
                                         <div class="col-form-submit d-grid mx-auto">
-                                            <button onfocus="this.blur()" onclick="return false;" type="submit" class="submit-btn  btn-primary ">Sign me up!</button>
+                                            <button onfocus="this.blur()" onclick="return false;" type="submit" class="submit-btn  btn-primary ">Sign me the guide!</button>
                                         </div>
                                     </div>
 
@@ -59,10 +59,10 @@
                 <div class="card">
                     <div class="card-body">
                         <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" preserveAspectRatio="xMidYMid meet" viewBox="0 0 24 24"><path fill="currentColor" d="m19.07 14.88l2.05 2.05L15.06 23H13v-2.06l6.07-6.06m1.97-1.75c.14 0 .27.06.38.17l1.28 1.28c.22.21.22.56 0 .77l-1 1l-2.05-2.05l1-1c.11-.11.25-.17.39-.17M17 4v6l-2-2l-2 2V4H9v16h2v2H7c-1.05 0-2-.95-2-2v-1H3v-2h2v-4H3v-2h2V7H3V5h2V4a2 2 0 0 1 2-2h12c1.05 0 2 .95 2 2v6l-2 2V4h-2M5 5v2h2V5H5m0 6v2h2v-2H5m0 6v2h2v-2H5Z"/></svg>
-                        <p class= "help-titles"><a href="https://elnacain.com/course/getpaid/"><strong>I want to land my first
+                        <p class= "help-titles"><a href="{{route('contact')}}"><strong>I want to land my first
                                     client</strong></a></p>
                     </div>
-                    <p class="Xcard-footer">Learn the first steps to find your first client <a href="https://elnacain.com/course/getpaid/">→</a></p>
+                    <p class="Xcard-footer">Learn the first steps to find your first client <a href="{{route('contact')}}">→</a></p>
                 </div>
                 <div class="card">
                     <div class="card-body">
@@ -76,10 +76,10 @@
                 <div class="card">
                     <div class="card-body">
                      <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" preserveAspectRatio="xMidYMid meet" viewBox="0 0 48 48"><mask id="svgIDa"><g fill="none" stroke="#fff" stroke-linecap="round" stroke-linejoin="round" stroke-width="4"><path fill="#fff" d="M8 8h32v20H8z"/><path d="M8 28L4 41h40l-4-13"/><path fill="#fff" d="M19.9 35h8.2l1.9 6H18l1.9-6Z"/></g></mask><path fill="currentColor" d="M0 0h48v48H0z" mask="url(#svgIDa)"/></svg>
-                        <p class= "help-titles"><strong><a href="https://elnacain.com/contact/">I'm looking to hire a writer</a></strong>
+                        <p class= "help-titles"><strong><a href="{{route('services')}}">I'm looking to hire a writer</a></strong>
                         </p>
                     </div>
-                    <p class="Xcard-footer">Want me to write for you?</br> Contact me! <a href="https://elnacain.com/contact/">→</a></p>
+                    <p class="Xcard-footer">Want me to write for you?</br> Contact me! <a href="{{route('services')}}">→</a></p>
                 </div>
             </div>
         </div>
@@ -146,7 +146,7 @@
             <div class="card">
                 <div class="card-body">
                     <h4 class="card-title">Leonard Rono</h4>
-                    <h6 class="text-primary mb-3"><a href="http://thepennyvoice.com">thepennyvoice.com</a></h6>
+                    <h6 class="text-primary  mb-3"><a href="http://thepennyvoice.com" class="text-color">thepennyvoice.com</a></h6>
 
                     <p class="card-text">
                     Catherine is an excellent Copywriter with an impressive understanding of topic clusters. Her ability to create content that converts played an integral role in our newly-launched blog getting a consistent number of readers.
@@ -157,7 +157,7 @@
             <div class="card">
                 <div class="card-body">
                     <h4 class="card-title">Leonard Rono</h4>
-                    <h6 class="text-primary mb-3"><a href="http://thepennyvoice.com">thepennyvoice.com</a></h6>
+                    <h6 class="text-primary mb-3"><a href="http://thepennyvoice.com" class="text-color">thepennyvoice.com</a></h6>
 
                     <p class="card-text">
                     Catherine is an excellent Copywriter with an impressive understanding of topic clusters. Her ability to create content that converts played an integral role in our newly-launched blog getting a consistent number of readers.
@@ -168,7 +168,7 @@
             <div class="card">
                 <div class="card-body">
                     <h4 class="card-title">Leonard Rono</h4>
-                    <h6 class="text-primary mb-3"><a href="http://thepennyvoice.com">thepennyvoice.com</a></h6>
+                    <h6 class="text-primary  mb-3"><a href="http://thepennyvoice.com" class="text-color">thepennyvoice.com</a></h6>
 
                     <p class="card-text">
                     Catherine is an excellent Copywriter with an impressive understanding of topic clusters. Her ability to create content that converts played an integral role in our newly-launched blog getting a consistent number of readers.
